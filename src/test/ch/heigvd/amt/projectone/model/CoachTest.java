855de066481bdb1bbd8b3ce5630989a1cc73dd90
Naguir <nair.alic@heig-vd.ch>
2019-11-11 21:27:55 +0100

    Fix bug in a simple test

--- src/test/ch/heigvd/amt/projectone/model/CoachTest.java
+++ src/test/ch/heigvd/amt/projectone/model/CoachTest.java
@@ -2,27 +2,20 @@ package ch.heigvd.amt.projectone.model;
 
 import org.junit.Test;
 
-import java.sql.Date;
-import java.util.LinkedList;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CoachTest {
-    private LinkedList<Player> players = new LinkedList<>();
-    private Team team;
 
     @Test
     public void createCoach(){
 
-        team = new Team("HEIG", "Yverdon", new Date(22051993));
-
-        players.add(new Player("Teklehaimanot", "Nair","AVG",22,team));
-        players.add(new Player("Alic","Robel","AVD",33,team));
-
-
         Coach coach = Coach.builder()
                 .lastName("Alic")
-                .firstName("Robel").build();
+                .firstName("Robel")
+                .username("robNal")
+                .password("test")
+                .isAdmin(true)
+                .build();
         assertNotNull(coach);
         assertEquals("Alic",coach.getLastName());
         assertEquals("Robel", coach.getFirstName());
